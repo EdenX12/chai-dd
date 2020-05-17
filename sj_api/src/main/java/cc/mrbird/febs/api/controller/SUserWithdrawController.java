19674a325e2c1c09a808ce4cdafc20d47b392ea4
彭巧 <2900691599@qq.com>
2020-05-17 10:43:38 +0800

    提现

--- sj_api/src/main/java/cc/mrbird/febs/api/controller/SUserWithdrawController.java
+++ sj_api/src/main/java/cc/mrbird/febs/api/controller/SUserWithdrawController.java
@@ -1,7 +1,11 @@
 package cc.mrbird.febs.api.controller;
 
+import cc.mrbird.febs.api.entity.SBank;
 import cc.mrbird.febs.api.entity.SUser;
+import cc.mrbird.febs.api.entity.SUserBank;
 import cc.mrbird.febs.api.entity.SUserWithdraw;
+import cc.mrbird.febs.api.service.ISBankService;
+import cc.mrbird.febs.api.service.ISUserBankService;
 import cc.mrbird.febs.api.service.ISUserService;
 import cc.mrbird.febs.api.service.ISUserWithdrawService;
 import cc.mrbird.febs.common.annotation.Log;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -35,12 +40,18 @@ public class SUserWithdrawController extends BaseController {
     @Autowired
     private ISUserService userService;
 
+    @Autowired
+    private ISUserBankService userBankService;
+
+    @Autowired
+    private ISBankService bankService;
+
     /**
      * 新增用户提现
      */
     @Log("新增用户提现")
     @PostMapping("/addUserWithdraw")
-    public FebsResponse addUserWithdraw(@Valid SUserWithdraw userWithdraw) {
+    public FebsResponse addUserWithdraw(@NotEmpty(message="用户银行卡ID不可为空") String userBankId, @NotEmpty(message="金额不可为空") String amount ) {
 
         FebsResponse response = new FebsResponse();
         response.put("code", 0);
@@ -48,18 +59,32 @@ public class SUserWithdrawController extends BaseController {
         try {
 
             SUser user = FebsUtil.getCurrentUser();
-            userWithdraw.setUserId(user.getId());
 
-            // 最低提现金额10元
-            if (userWithdraw.getAmount().compareTo(BigDecimal.valueOf(10)) < 0) {
-                message = "提现金额最低10元起！";
+            SUserBank userBank = userBankService.getById(userBankId);
+            if(userBank == null){
+                message = "银行卡不存在！";
                 response.put("code", 1);
                 response.message(message);
                 return response;
             }
 
+            if(!user.getId().equals(userBank.getUserId())){
+                message = "当前用户不能提现到该卡，请核对后再提交！";
+                response.put("code", 1);
+                response.message(message);
+                return response;
+            }
+
+            BigDecimal realAmount = new BigDecimal(amount);
+            // 最低提现金额10元
+            if (realAmount.compareTo(BigDecimal.valueOf(10)) < 0) {
+                message = "提现金额最低10元起！";
+                response.put("code", 1);
+                response.message(message);
+                return response;
+            }
             // 用户提现金额大于余额
-            if (userWithdraw.getAmount().compareTo(user.getTotalAmount()) > 0) {
+            if (realAmount.compareTo(user.getTotalAmount()) > 0) {
 
                 message = "提现金额不能超过余额！";
                 response.put("code", 1);
@@ -67,13 +92,21 @@ public class SUserWithdrawController extends BaseController {
                 return response;
             }
 
-            userWithdraw.setCreateTime(new Date());
-            userWithdraw.setStatus(0);
+            SBank bank = bankService.getById(userBank.getBankId());
 
+            SUserWithdraw userWithdraw = new SUserWithdraw();
+            userWithdraw.setUserId(user.getId());
+            userWithdraw.setAmount(realAmount);
+            userWithdraw.setBankName(bank.getBankName());
+            userWithdraw.setBankCode(bank.getBankCode());
+            userWithdraw.setRealName(userBank.getRealName());
+            userWithdraw.setIdCard(userBank.getIdCard());
+            userWithdraw.setCardNum(userBank.getCardNum());
+            userWithdraw.setStatus(0);
+            userWithdraw.setCreateTime(new Date());
             this.userWithdrawService.save(userWithdraw);
 
             // 用户冻结金额追加
-            user.setLockAmount(user.getLockAmount().add(userWithdraw.getAmount()));
             user.setTotalAmount(user.getTotalAmount().subtract(userWithdraw.getAmount()));
             this.userService.updateById(user);
 
