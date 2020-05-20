cc8a7c743eb79d0e1a5815de8d51cb3bcebed878
liunb <liunianbo@51kangyin.com>
2020-05-20 22:44:51 +0800

    Bug

--- sj_api/src/main/java/cc/mrbird/febs/api/controller/SUserTaskController.java
+++ sj_api/src/main/java/cc/mrbird/febs/api/controller/SUserTaskController.java
@@ -516,8 +516,10 @@ public class SUserTaskController extends BaseController {
      * 2分钟执行一次 (支付失败时间超过5分钟的任务处理)
      */
     @Scheduled(cron = "0 */2 * * * ?")
-    @Transactional
     public void unLockPayFailTask() {
+
+        // 同时把s_user_task_line中的相关数据也同样修改为 3-不支付[取消或超期]
+        userTaskService.updateUserTaskLineFailBatch();
 
         // 抽取s_user_task中 支付状态（锁定） 支付时间大于5分钟的 数据 修改状态为 3-不支付[取消或超期]
         this.userTaskService.updateTaskForUnLock();
@@ -525,9 +527,6 @@ public class SUserTaskController extends BaseController {
         // 再根据s_user_task_line中的task_line_id到 表s_task_line 修改 冻结任务数量-1
         Integer count = userTaskService.updateTaskLineFailBatch();
         log.info("定时任务本次解冻了"+count+"个任务线");
-
-        // 同时把s_user_task_line中的相关数据也同样修改为 3-不支付[取消或超期]
-        userTaskService.updateUserTaskLineFailBatch();
     }
 
     /**
